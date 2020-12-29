42031cf28f1fd08ec4f6d88a65da960a0e364e1a
Muneeb Ur Rehman <muneeb.github@gmail.com>
2020-12-29 10:43:08 +0500

    completed matrix transpose problem

--- CH1/RotateMatrix.java
+++ CH1/RotateMatrix.java
@@ -1,50 +1,29 @@
 package CH1;
 
-//TODO: Resolve inner box bugs
 public class RotateMatrix {
     public void solution(int[][] mat) {
-        int start=0;
-        for(int i=mat[0].length-1;i>0;i=i-2){
-            rotateBox(mat,i,i,start,start);
-            start++;
+        //take transpose
+        int n=mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
         }
-    }
+        //flip
 
-    private void rotateBox(int[][] box, int lastRow,int lastCol, int startRow, int startCol) {
-
-        for(int i=startRow;i<=lastRow;i++){
-            for(int j=startCol;j<=lastCol;j++){
-                System.out.print(box[i][j]+" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n/2); j++) {
+               int temp=mat[i][j];
+               mat[i][j]=mat[i][n-1-j];
+               mat[i][n-1-j]=temp;
             }
-            System.out.println();
-        }
-        int nextColIndex = lastCol;
-        int nextRowIndex = startRow;
-        int storedNum = box[startRow][startCol];
-        boolean rowTurn=true;
-
-        for (int i = 0; i <= (lastRow-startRow)*4; i++) {
-            int temp = box[nextRowIndex][nextColIndex];
-            box[nextRowIndex][nextColIndex] = storedNum;
-            storedNum=temp;
-            rowTurn=!rowTurn;
-            if(i!=0 && i%4==0){
-                nextRowIndex++;
-                storedNum=box[nextRowIndex][nextColIndex];
-            }
-            if(rowTurn){
-                int tempCol=nextColIndex;
-                nextColIndex=lastCol-nextRowIndex;
-                nextRowIndex=tempCol;
-
-            }else {
-                int tempRow=nextRowIndex;
-                nextRowIndex=nextColIndex;
-                nextColIndex=lastRow-tempRow;
-            }
-
         }
 
     }
+
+
+
 
 }
